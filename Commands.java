30630ce96a1a7b30a925e3b56e1fd6a1eb3c12f3
melodicMorphine <88890567+melodicMorphine@users.noreply.github.com>
2022-05-08 19:17:33 -0300

    Update Commands.java

--- Commands.java
+++ Commands.java
@@ -1,1 +1,11 @@
 
+package chat;
+
+public class Commands {
+    public static final String USER_LIST = "::LISTA_CLIENTES";
+    public static final String SAIR = "::SAIR";
+    public static final String MESSAGE = "::MSG";
+    public static final String LOGIN = "::LOGIN";
+    public static final String LOGIN_ACCEPTED = "::LOGIN_ACEITO";
+    public static final String LOGIN_DENIED = "::LOGIN_DENIED";
+}
